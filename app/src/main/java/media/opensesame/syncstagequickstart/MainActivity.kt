a592b50acf0c1ad2ed7737699302e2dbf6929800
Patryk Kaczmarek <patryk.kaczmarek@wp.eu>
2022-05-15 20:26:57 +0200

    updated permissions requests, updated sdk package to 0.12.0

--- app/src/main/java/media/opensesame/syncstagequickstart/MainActivity.kt
+++ app/src/main/java/media/opensesame/syncstagequickstart/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
@@ -38,6 +39,12 @@ class MainActivity : ComponentActivity() {
     var accessToken = ""
     var userId = 0
     private var sdk: SyncStage? = null
+
+    private val requestMultiplePermissions =  registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+        permissions.entries.forEach {
+            Log.e(TAG, "TEST ${it.key} = ${it.value}")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -87,6 +94,8 @@ class MainActivity : ComponentActivity() {
             val gsonPretty = GsonBuilder().setPrettyPrinting().create()
             val connectionDataString: String = gsonPretty.toJson(connectionData)
             showToast(connectionDataString)
+        } else {
+            showToast("SDK not initialized.")
         }
         return
     }
@@ -121,37 +130,26 @@ class MainActivity : ComponentActivity() {
             accessToken = token
         }
     ) {
-        val launcher = rememberLauncherForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
-            if (isGranted) {
-                // Permission Accepted: Do something
-                Log.d(TAG, "PERMISSION GRANTED")
-
-            } else {
-                // Permission Denied: Do something
-                Log.d(TAG, "PERMISSION DENIED")
-            }
-        }
-
         LockScreenOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
 
+        var recordAudioGranted by remember { mutableStateOf(false) }
+        checkPermission(Manifest.permission.RECORD_AUDIO, onGranted = { recordAudioGranted = true })
+        var permissionsGranted = recordAudioGranted
+
+        var usersDropdownExpanded by remember { mutableStateOf(false) }
+
+        val userIds: List<Int> = listOf(0, 1, 2, 3, 4, 5, 6)
+        var selectedUserId by remember { mutableStateOf(initialUserId) }
+        var token by remember { mutableStateOf(initialDeveloperToken) }
+
         Surface(color = MaterialTheme.colors.background) {
-            var recordAudioGranted by remember { mutableStateOf(false) }
-            var internetGranted by remember { mutableStateOf(false) }
-            val permissionsGranted = recordAudioGranted && internetGranted
-            var usersDropdownExpanded by remember { mutableStateOf(false) }
-
-            val userIds: List<Int> = listOf(0, 1, 2, 3, 4, 5, 6)
-            var selectedUserId by remember { mutableStateOf(initialUserId) }
-            var token by remember { mutableStateOf(initialDeveloperToken) }
-
             Column(
                 modifier = Modifier
                     .fillMaxSize()
             ){
                 Row(
-                    verticalAlignment = Alignment.CenterVertically
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.height(120.dp)
                 ) {
                     Image(
                         modifier = Modifier.scale(0.7f),
@@ -182,7 +180,7 @@ class MainActivity : ComponentActivity() {
                         },
                         placeholder = { Text("Access token") },
                     )
-
+                    Spacer(modifier=Modifier.height(8.dp))
                     Row(modifier = Modifier.fillMaxWidth()) {
                         var textfieldSize by remember { mutableStateOf(Size.Zero) }
                         OutlinedTextField(
@@ -192,8 +190,10 @@ class MainActivity : ComponentActivity() {
                                 .onGloballyPositioned { coordinates ->
                                     //This value is used to assign to the DropDown the same width
                                     textfieldSize = coordinates.size.toSize()
-                                },
+                                }
+                                .clickable{ usersDropdownExpanded = !usersDropdownExpanded },
                             readOnly = true,
+                            enabled = false,
                             onValueChange = { },
                             trailingIcon = {
                                 Icon(
@@ -208,6 +208,12 @@ class MainActivity : ComponentActivity() {
                                     }
                                 )
                             },
+                            colors = TextFieldDefaults.outlinedTextFieldColors(
+                                focusedBorderColor = Color.Black,
+                                unfocusedBorderColor = Color.Black,
+                                textColor = Color.Black,
+                                disabledTextColor = Color.Black
+                            )
                         )
 
                         DropdownMenu(
@@ -234,23 +240,14 @@ class MainActivity : ComponentActivity() {
 
                     Spacer(modifier = Modifier.height(10.dp))
                     Button(modifier = Modifier.fillMaxWidth(), onClick = {
-                        // Check permission
-                        requestPermission(
-                            launcher,
-                            Manifest.permission.RECORD_AUDIO,
-                            onGranted = { recordAudioGranted = true })
+                        requestMultiplePermissions.launch(
+                            arrayOf(
+                                Manifest.permission.RECORD_AUDIO
+                            ),
+                        )
+                        recordAudioGranted = true
                     }) {
-                        Text("Request RECORD_AUDIO permission")
-                    }
-                    Spacer(modifier = Modifier.height(10.dp))
-                    Button(modifier = Modifier.fillMaxWidth(), onClick = {
-                        // Check permission
-                        requestPermission(
-                            launcher,
-                            Manifest.permission.INTERNET,
-                            onGranted = { internetGranted = true })
-                    }) {
-                        Text("Request INTERNET permission")
+                        Text("Request permissions")
                     }
 
                     Spacer(modifier = Modifier.height(10.dp))
@@ -311,23 +308,14 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun requestPermission(
-        launcher: ManagedActivityResultLauncher<String, Boolean>,
-        permission: String,
-        onGranted: () -> Unit = {}
-    ) {
-        when (PackageManager.PERMISSION_GRANTED) {
-            ContextCompat.checkSelfPermission(
+    private fun checkPermission(permission: String, onGranted: () -> Unit = {}){
+        if (ContextCompat.checkSelfPermission(
                 applicationContext,
                 permission
-            ) -> {
-                showToast("$permission permission granted")
-                onGranted()
-            }
-            else -> {
-                // Asking for permission
-                launcher.launch(permission)
-            }
+            ) == PackageManager.PERMISSION_GRANTED){
+
+            Log.d(TAG, "$permission permission granted")
+            onGranted()
         }
     }
 
